34cc3bd39971fb615b21fd884d9ac4caf98c392e
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-10-04 10:32:46 +0200

    9

---
+++ ejercicio9.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$fechaNacimiento = date_create("2003-02-25");
+
+$dia = date_format($fechaNacimiento, "d");
+$mes = date_format($fechaNacimiento, "m");
+$year = date_format($fechaNacimiento, "Y");
+
+$diaActual = date("d");
+$mesActual = date("m");
+$yearActual = date("Y");
+
+$edadYear = $yearActual - $year;
+
+echo $dia . "/" . $mes . "/" . $year . "<br>";
+
+$year_diferencia  = $yearActual - $year;
+$mes_diferencia = $mesActual - $mes;
+$dia_diferencia   = $diaActual - $dia;
+
+if ($dia_diferencia < 0 || $mes_diferencia < 0) {
+  $year_diferencia--;
+}
+
+if ($edadYear >= 18) {
+    echo "Puede pasar dentro";
+} else if ($edadYear < 18) {
+    echo "No puede pasar";
+} else if ($edadYear > 65) {
+    echo "Es demasiado mayor para entrar";
+}
+
+if ($year_diferencia >= 18) {
+    echo "Puede pasar dentro";
+} else if ($year_diferencia < 18) {
+    echo "No puede pasar";
+} else if ($year_diferencia > 65) {
+    echo "Es demasiado mayor para entrar";
+}
+
+?>
+</body>
+</html>
