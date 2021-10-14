089ca00015c61976813a74a311c07784f5495fd8
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-10-14 12:08:39 +0200

    18

--- ejercicio18.php
+++ ejercicio18.php
@@ -7,7 +7,51 @@
     <title>Document</title>
 </head>
 <body>
-<?php include_once 'datos18.php';?>
+<?php
+$errorPoblacion = "";
+$errorCodigoPostal = "";
+$errorProvincia = "";
+$poblacion = "";
+$codigoPostal = "";
+$provincia = "";
+$descripcionMunicipio = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $poblacion = $_POST['poblacion'];
+    $codigoPostal = $_POST['codigoPostal'];
+
+    if (isset($_POST['provincia'])) {
+        $provincia = $_POST['provincia'];
+    }
+
+    if (empty($poblacion)) {
+        $errorPoblacion = "Indique una población.";
+    }
+
+    if (empty($codigoPostal)) {
+        $errorCodigoPostal = "Indique un código postal.";
+    }
+
+    if (empty($provincia)) {
+        $errorProvincia = "Indique una provincia.";
+    }
+
+    //$rutaImagen = $_FILES['imagen']['name'];
+    
+    $descripcionMunicipio = $_POST['descripcionMunicipio'];
+
+    // Hacemos seguro el campo de Descripcion del municipio.
+    $descripcionMunicipio = stripslashes($descripcionMunicipio);
+    $descripcionMunicipio = strip_tags($descripcionMunicipio);
+    $descripcionMunicipio = htmlspecialchars($descripcionMunicipio);
+
+    if (!empty($poblacion) && !empty($codigoPostal) && !empty($provincia)) {
+        header("Location: correcto.php");
+    }
+
+}
+?>
     <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST" enctype="multipart/form-data">
         <fieldset>
             <legend>Formulario</legend>
