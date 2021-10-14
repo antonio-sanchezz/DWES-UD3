4c3396158e47a1aa198c3cf999c8b1720099ae1f
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-10-14 12:02:31 +0200

    18

--- ejercicio18.php
+++ ejercicio18.php
@@ -7,39 +7,44 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="correcto.php" method="POST" enctype="multipart/form-data">
+<?php include_once 'datos18.php';?>
+    <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST" enctype="multipart/form-data">
         <fieldset>
             <legend>Formulario</legend>
             <p>
                 <label for="poblacion">Población</label>
-                <input type="text" name="poblacion"/>
+                <input type="text" name="poblacion" value="<?php echo $poblacion?>"/>
+                <span style="color:red"><?php echo $errorPoblacion?></span>
             </p>
             <p>
                 <label for="codigoPostal">Código Postal</label>
-                <input type="number" name="codigoPostal"/>
+                <input type="number" name="codigoPostal" value="<?php echo $codigoPostal?>"/>
+                <span style="color:red"><?php echo $errorCodigoPostal?></span>
             </p>
             <p>
-                <input type="radio" id="wifi" name="provincia" value="wifi"/>
-                <label for="html">Wifi</label>
-                <input type="radio" id="cable" name="provincia" value="cable"/>
-                <label for="javascript">Cable</label>
-                <input type="radio" id="fibra" name="provincia" value="fibra"/>
-                <label for="javascript">Fibra</label>
-                <input type="radio" id="fibra" name="provincia" value="fibra"/>
-                <label for="javascript">Fibra</label>
-                <input type="radio" id="fibra" name="provincia" value="fibra"/>
-                <label for="javascript">Fibra</label>
-                <input type="radio" id="fibra" name="provincia" value="fibra"/>
-                <label for="javascript">Fibra</label>
-                <input type="radio" id="fibra" name="provincia" value="fibra"/>
-                <label for="javascript">Fibra</label>
-                <input type="radio" id="fibra" name="provincia" value="fibra"/>
-                <label for="javascript">Fibra</label>
+                <input type="radio" id="sevilla" name="provincia" value="sevilla" <?php if ( $provincia == "sevilla"){ echo "checked";}?>/>
+                <label for="sevilla">Sevilla</label>
+                <input type="radio" id="cadiz" name="provincia" value="cadiz" <?php if ( $provincia == "cadiz"){ echo "checked";}?>/>
+                <label for="cadiz">Cádiz</label>
+                <input type="radio" id="cordoba" name="provincia" value="cordoba" <?php if ( $provincia == "cordoba"){ echo "checked";}?>/>
+                <label for="cordoba">Córdoba</label>
+                <input type="radio" id="granada" name="provincia" value="granada" <?php if ( $provincia == "granada"){ echo "checked";}?>/>
+                <label for="granada">Granada</label>
+                <input type="radio" id="huelva" name="provincia" value="huelva" <?php if ( $provincia == "huelva"){ echo "checked";}?>/>
+                <label for="huelva">Huelva</label>
+                <input type="radio" id="jaen" name="provincia" value="jaen" <?php if ( $provincia == "jaen"){ echo "checked";}?>/>
+                <label for="jaen">Jaén</label>
+                <input type="radio" id="malaga" name="provincia" value="malaga" <?php if ( $provincia == "malaga"){ echo "checked";}?>/>
+                <label for="malaga">Málaga</label>
+                <input type="radio" id="almeria" name="provincia" value="almeria" <?php if ( $provincia == "almeria"){ echo "checked";}?>/>
+                <label for="almeria">Almería</label>
+                <span style="color:red"><?php echo $errorProvincia?></span>
             </p>
             <fieldset>
             <legend>Descripción del municipio</legend>
             <span style="color:red"></span>
             <textarea id="descripcionMunicipio" name="descripcionMunicipio" rows="4" cols="50">
+            <?php echo $descripcionMunicipio?>    
             </textarea>
         </fieldset>
         <p>
